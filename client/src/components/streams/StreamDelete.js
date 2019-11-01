8de207410fdc7b18fc77fd123a66740b7cc985ce
Maxim Didenko <didenko.mn@gmail.com>
2019-11-01 16:56:36 +0200

    Added modal using fragment

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
-  return (<div>Stream Delete</div>);
+  const actions = (
+    <React.Fragment>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      Stream Delete
+      <Modal
+        title="Delete Stream"
+        actions={actions}
+      >
+        Are you sure you want to delete this stream?
+      </Modal>
+    </div>
+  );
 };
 
 export default StreamDelete;
